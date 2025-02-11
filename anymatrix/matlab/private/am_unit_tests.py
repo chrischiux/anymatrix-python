7897e96833647627c66c31f1067c0ca8e44a16b2
Chris Chiu <chrischiu0509@gmail.com>
2025-02-11 01:24:37 +0000

    added unit test for matlab group

--- anymatrix/matlab/private/am_unit_tests.py
+++ anymatrix/matlab/private/am_unit_tests.py
@@ -1,3 +1,11 @@
 def test_matlab_toeplitz(am):
     A = am.anymatrix('matlab/toeplitz',[1, 1, 1], [1, 2, 2])
     anymatrix_check_props(am, A, 'matlab/toeplitz')
+
+def test_matlab_hadamard(am):
+    A = am.anymatrix('matlab/hadamard', 2)
+    anymatrix_check_props(am, A, 'matlab/hadamard')
+    A = am.anymatrix('matlab/hadamard', 4)
+    anymatrix_check_props(am, A, 'matlab/hadamard')
+    A = am.anymatrix('matlab/hadamard', 40)
+    anymatrix_check_props(am, A, 'matlab/hadamard')

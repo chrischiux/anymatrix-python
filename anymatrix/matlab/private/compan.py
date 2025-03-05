edf69ee6160a69d73186c5bbf21c4fba44fd2f95
Chris Chiu <sc22phc@leeds.ac.uk>
2025-03-05 19:54:09 +0000

    reimplemented compan. new validation test added

--- anymatrix/matlab/private/compan.py
+++ anymatrix/matlab/private/compan.py
@@ -3,10 +3,29 @@ import numpy as np
 
 def compan(u):
 
-    if type(u) != np.ndarray:
+    if type(u) == int or type(u) == float:
         return np.array([])
     
-    return rogues_compan(u)
+    if type(u) != np.ndarray:
+        u = np.array(u)
+    
+    n = np.size(u)
+    if n <= 1:
+        return np.array([])
+
+    n = len(u)
+    if n < 2:
+        raise ValueError("Input vector must have at least two elements")
+    
+    A = np.zeros((n-1, n-1), dtype=float)
+    if n > 1:
+        A[0, :] = -u[1:] / u[0]
+        A[1:, :-1] = np.eye(n-2)
+    
+    if A.shape == (1,1):
+        return A[0,0]
+    
+    return A
  
 # Copy docstring from original function
 compan.__doc__ = rogues_compan.__doc__
--- verify2.py
+++ verify2.py
@@ -4,12 +4,13 @@ import matlab.engine
 from anymatrix import Anymatrix
 np.set_printoptions(suppress=True)
 am = Anymatrix()
-python_data = am.anymatrix("matlab/pascal", 30)[28,29]
+python_data = am.anymatrix("matlab/pascal", 31)[30,30]
 print(python_data)
 
-# eng = matlab.engine.start_matlab()
-# eng.cd(r"C:\Users\propo\OneDrive - University of Leeds\Documents\MATLAB\anymatrix", nargout=0)
-# ans = eng.anymatrix("matlab/pascal", 30)
-# converted = np.array(ans)
-# matlab_data = converted[28,29]
-# print(matlab_data)
+eng = matlab.engine.start_matlab()
+eng.cd(r"C:\Users\propo\OneDrive - University of Leeds\Documents\MATLAB\anymatrix", nargout=0)
+ans = eng.anymatrix("matlab/pascal", 30)
+converted = np.array(ans)
+matlab_data = converted[28,29]
+print(matlab_data)
+

acf057a6ae68d9045816135522b0e7fccd7a3ecf
Chris Chiu <sc22phc@leeds.ac.uk>
2025-04-25 15:14:14 +0100

    fixed error for empty array check

--- anymatrix/testing/private/test_square.py
+++ anymatrix/testing/private/test_square.py
@@ -1,6 +1,8 @@
 import numpy as np
 def test_square(M):
     """Returns true if number of rows equals number of columns in the given matrix M"""
+    if M.size == 0:
+        return True
     if M.shape[0] == M.shape[1]:
         return True
     else:
